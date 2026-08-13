d715828bf1ec8d799d19340b32712a269cbc103f
João Cesar <joao.cesar@dbaonline.com.br>
2026-08-13 15:24:39 +0000

    Adiciona migration que restringe exclusao de cargo em uso

--- Migrations/20260813152308_RestringeExclusaoDeCargoEmUso.Designer.cs
+++ Migrations/20260813152308_RestringeExclusaoDeCargoEmUso.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoToGrow.Context;
 
@@ -11,9 +12,11 @@ using ProjetoToGrow.Context;
 namespace ProjetoToGrow.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260813152308_RestringeExclusaoDeCargoEmUso")]
+    partial class RestringeExclusaoDeCargoEmUso
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260813152308_RestringeExclusaoDeCargoEmUso.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoToGrow.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestringeExclusaoDeCargoEmUso : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pessoas_Cargos_CargoId",
+                table: "Pessoas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pessoas_Cargos_CargoId",
+                table: "Pessoas",
+                column: "CargoId",
+                principalTable: "Cargos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pessoas_Cargos_CargoId",
+                table: "Pessoas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pessoas_Cargos_CargoId",
+                table: "Pessoas",
+                column: "CargoId",
+                principalTable: "Cargos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
